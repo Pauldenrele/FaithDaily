2547901c306ec9ffaa9b2e5e9bf4b3ab038b660a
adenrelepaul <pauldenrele@gmail.com>
2020-10-17 05:43:30 +0100

    Navigation setup

---
+++ app/src/main/java/com/bibleapp/faithdaily/ui/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.bibleapp.faithdaily.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bibleapp.faithdaily.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
---
+++ app/src/main/java/com/bibleapp/faithdaily/ui/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package com.bibleapp.faithdaily.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bibleapp.faithdaily.R
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+}
